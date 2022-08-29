56f0d26c97f030ee6d419f81fa3add203f980a9a
Valentina Llanos Pontaut <105258399+valenpontaut@users.noreply.github.com>
2022-08-29 20:20:27 -0300

    Update models.py

--- database/models.py
+++ database/models.py
@@ -97,7 +97,7 @@ class Movimientos(models.Model):
 class Prestamo(models.Model):
     loan_id = models.AutoField(primary_key=True)
     loan_type = models.TextField()
-    loan_date = models.DateField()
+    loan_date = models.TextField()
     loan_total = models.IntegerField()
     customer_id = models.IntegerField()
 
